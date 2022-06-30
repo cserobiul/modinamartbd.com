0efa1105f4d8d346d603b4b0db12dccb714ff132
cserobiul <abishar14@gmail.com>
2022-06-30 17:55:58 +0600

    index2 category section dynamic

--- resources/views/frontend/home/index2/_categories_with_sub.blade.php
+++ resources/views/frontend/home/index2/_categories_with_sub.blade.php
@@ -3,188 +3,35 @@
         <div class="row">
             <div class="col-12 pr-0">
                 <div class="category-slide5two no-arrow">
+                    @foreach($categories as $category)
                     <div>
                         <div class="category-box">
                             <div class="img-wrraper">
-                                <a href="category-page(left-sidebar).html">
-                                    <img src="{{ asset('frontend') }}/assets/images/mega-store/category/1.jpg" alt="category" class="img-fluid">
+                                <a href="{{ route('categoryDetailsSlug',$category->slug) }}">
+                                    @if($category->photo)
+                                        <img src="{{ asset($category->photo) }}" alt="category" class="img-fluid">
+                                    @else
+                                        <img src="{{ asset('frontend/assets/images/mega-store/category/1.jpg') }}" alt="category" class="img-fluid">
+                                    @endif
                                 </a>
                             </div>
                             <div class="category-detail">
-                                <a href="category-page(left-sidebar).html">
-                                    <h2>Fashion & Clothing</h2>
+                                <a href="{{ route('categoryDetailsSlug',$category->slug) }}">
+                                    <h2>{{ $category->name }}</h2>
                                 </a>
                                 <ul>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">Winter Jackets  </a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html"> Office Blazer </a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html"> sports watches </a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html"> Professional Cap </a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">printed t-shirt </a>
-                                    </li>
+                                    @foreach($category->children as $child)
+                                        <li>
+                                            <a href="{{ route('categoryDetailsSlug',$child->slug) }}">{{ $child->name }}</a>
+                                        </li>
+                                    @endforeach
                                 </ul>
-                                <a href="category-page(left-sidebar).html" class="btn btn-rounded btn-md btn-block">shop now</a>
+                                <a href="{{ route('categoryDetailsSlug',$category->slug) }}" class="btn btn-rounded btn-md btn-block">shop now</a>
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <div class="category-box">
-                            <a href="category-page(left-sidebar).html">
-                                <img src="{{ asset('frontend') }}/assets/images/mega-store/category/2.jpg" alt="category" class="img-fluid">
-                            </a>
-                            <div class="category-detail">
-                                <a href="category-page(left-sidebar).html">
-                                    <h2>home furniture</h2>
-                                </a>
-                                <ul>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">Chesterfield Chair</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html"> Bunching Table</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html"> Sofa beds</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html"> Sleeper Sofas</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html"> Storage cabinets </a>
-                                    </li>
-                                </ul>
-                                <a href="category-page(left-sidebar).html" class="btn btn-rounded btn-md btn-block">shop now</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="category-box">
-                            <a href="category-page(left-sidebar).html">
-                                <img src="{{ asset('frontend') }}/assets/images/mega-store/category/3.jpg" alt="category" class="img-fluid">
-                            </a>
-                            <div class="category-detail">
-                                <a href="category-page(left-sidebar).html">
-                                    <h2>letest electronics</h2>
-                                </a>
-                                <ul>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">Sony  oled TV </a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">Gaming Phone</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">Asus Zenbook Pro</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">lg 21:9 Monitor </a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">Ceiling Fans</a>
-                                    </li>
-                                </ul>
-                                <a href="category-page(left-sidebar).html" class="btn btn-rounded btn-md btn-block">shop now</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="category-box">
-                            <a href="category-page(left-sidebar).html">
-                                <img src="{{ asset('frontend') }}/assets/images/mega-store/category/4.jpg" alt="category" class="img-fluid">
-                            </a>
-                            <div class="category-detail">
-                                <a href="category-page(left-sidebar).html">
-                                    <h2>beauty & cosmetics</h2>
-                                </a>
-                                <ul>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">Skin Concealer</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">eye makeup mascara</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">Skin Cleansing Cream</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">organic cream </a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">body loation</a>
-                                    </li>
-                                </ul>
-                                <a href="category-page(left-sidebar).html" class="btn btn-rounded btn-md btn-block">shop now</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="category-box">
-                            <a href="category-page(left-sidebar).html">
-                                <img src="{{ asset('frontend') }}/assets/images/mega-store/category/5.jpg" alt="category" class="img-fluid">
-                            </a>
-                            <div class="category-detail">
-                                <a href="category-page(left-sidebar).html">
-                                    <h2>fresh grocery</h2>
-                                </a>
-                                <ul>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">White Pepper Powder </a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">vegetable & fruits</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">Tomato sauce</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">Barbecue sauce</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">fresh coffee powder</a>
-                                    </li>
-                                </ul>
-                                <a href="category-page(left-sidebar).html" class="btn btn-rounded btn-md btn-block">shop now</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="category-box">
-                            <a href="category-page(left-sidebar).html">
-                                <img src="{{ asset('frontend') }}/assets/images/mega-store/category/2.jpg" alt="category" class="img-fluid">
-                            </a>
-                            <div class="category-detail">
-                                <a href="category-page(left-sidebar).html">
-                                    <h2>home furniture</h2>
-                                </a>
-                                <ul>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html">Chesterfield Chair</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html"> Bunching Table</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html"> Sofa beds</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html"> Sleeper Sofas</a>
-                                    </li>
-                                    <li>
-                                        <a href="category-page(left-sidebar).html"> Storage cabinets </a>
-                                    </li>
-                                </ul>
-                                <a href="category-page(left-sidebar).html" class="btn btn-rounded btn-md btn-block">shop now</a>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
+
                 </div>
             </div>
         </div>
